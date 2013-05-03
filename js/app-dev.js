d6735f644a9c8718b922a2d39adaab0ad33e412f
Ryan P LeTulle <bayousoft@gmail.com>
2013-05-03 17:03:42 -0500

    TS

--- js/app-dev.js
+++ js/app-dev.js
@@ -15,7 +15,7 @@ function route(event) {
         var data = {
         img: "buildbot.jpg",
         name: "Build Bot",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+        description: "<iframe width='350' height='199' src='http://www.youtube.com/v/K_LlaAuPfYo' frameborder='0' allowfullscreen></iframe>"
         };
 
         var template = $('#pageTpl').html().trim();
